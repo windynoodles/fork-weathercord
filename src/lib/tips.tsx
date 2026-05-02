7a616a6f3ac4c7a0c2ed359a1484a071972264d9
raynecloudy <hi@rayne.page>
2026-05-01 17:21:31 -0700

    fix tip wording

--- src/lib/tips.tsx
+++ src/lib/tips.tsx
@@ -3,12 +3,12 @@ import { ReactNode } from "react";
 type Tip = string | ReactNode | (() => string | ReactNode);
 
 export const tips: Tip[] = [
-  "Clicking a username in a profile popup toggles between username and ID.",
+  "Clicking the username in profile cards toggles between username and ID.",
   <span>In computer science, <code>NaN</code> (Not a Number) is classified as a number.</span>,
   "Let's get this show on the road!",
-  "Mention via ID by prefixing it with @&",
+  "Mention with an ID by prefixing it with @&",
   "Perfect little packages, disposed of when it's finished",
-  <span>Weathercord is open-source. <a href="https://github.com/raynecloudy/weathercord" target="_blank"><u>https://github.com/raynecloudy/weathercord</u></a></span>,
+  <span>Weathercord is open source. <a href="https://github.com/raynecloudy/weathercord" target="_blank"><u>https://github.com/raynecloudy/weathercord</u></a></span>,
   "you look so pretty today!"
 ];
 
